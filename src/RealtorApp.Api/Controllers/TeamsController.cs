5764af052a2d0f963806c02a05e92175eb3a93f7
stew <stewartanoya@gmail.com>
2025-12-28 08:19:09 -0500

    start teams controller

---
+++ src/RealtorApp.Api/Controllers/TeamsController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RealtorApp.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : RealtorApiBaseController
+    {
+
+    }
+}
